6286733cd41f267417228ae24030d07b7125b07e
rigobertopo <rperez@cobach.edu.mx>
2022-10-21 16:58:22 -0500

    se crea paginas principales

---
+++ blog.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi portafolio - Blog</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <header>
+        <nav id="navbar">
+            <div class="Contenedor">
+                <img src="img/Escudo_unach.png" alt="Logo" class="Logo">
+                <ul class="Texto-menu">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="quiensoy.php">¿Quién soy?</a></li>
+                    <li><a href="portafolio.php">Portafolio</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="contacto.php">Contácto</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+    </header>
+    <article>
+        <h1>Blog</h1>
+    </article>  
+    <footer>
+    <p class="texto">Licenciatura en Sistemas Computacionales</p>
+    </footer>  
+</body>
+</html>
